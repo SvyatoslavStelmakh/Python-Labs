703a469dbb8b11dfcc1784f461d6cf5938997e8a
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-09-28 01:59:47 +0300

    выполнил 3 номер

--- Laba2/Laba2_1/task2.py
+++ Laba2/Laba2_1/task2.py
@@ -1,42 +1,42 @@
 input_data = input("Введите числа через пробел: ")
 
-num_words = input_data.split()
-numbers = []
+words_list = input_data.split()
+num_list = []
 
-for number in num_words:
+for number in words_list:
     if '.' in number:
-        numbers.append(float(number))
+        num_list.append(float(number))
     else:
-        numbers.append(int(number))
+        num_list.append(int(number))
 
-unique_numbers = list(set(numbers))
+unique_numbers = list(set(num_list))
 print("1. Уникальные числа: ", unique_numbers)
 
 repeating_numbers = []
-for i in range(len(numbers)):
-    counter = numbers.count(numbers[i])
+for i in range(len(num_list)):
+    counter = num_list.count(num_list[i])
     if counter > 1:
-        counter = repeating_numbers.count(numbers[i])
+        counter = repeating_numbers.count(num_list[i])
         if counter == 0:
-            repeating_numbers.append(numbers[i])
+            repeating_numbers.append(num_list[i])
 print("2. Повторяющиеся числа: ", repeating_numbers)            
 
-even_numbers = [num for num in numbers if isinstance(num, int) and num % 2 == 0]
-odd_numbers = [num for num in numbers if isinstance(num, int) and num % 2 != 0]
+even_numbers = [num for num in num_list if isinstance(num, int) and num % 2 == 0]
+odd_numbers = [num for num in num_list if isinstance(num, int) and num % 2 != 0]
 print("3. Чётные числа: ", repeating_numbers)
 print("4. Нечётные числа: ", repeating_numbers)
 
-negative_numbers = [num for num in numbers if num < 0]
+negative_numbers = [num for num in num_list if num < 0]
 print("5. Отрицательные числа: ", repeating_numbers)
     
-float_numbers = [num for num in numbers if isinstance(num, float)]
+float_numbers = [num for num in num_list if isinstance(num, float)]
 print("6. Числа с плавающей точкой: ", repeating_numbers)
     
-sum_multiple_5 = sum(num for num in numbers if isinstance(num, int) and num % 5 == 0)
+sum_multiple_5 = sum(num for num in num_list if isinstance(num, int) and num % 5 == 0)
 print("7. Сумма чисел, кратных 5: ", repeating_numbers)
     
-max_number = max(numbers) if numbers else None
+max_number = max(num_list) if num_list else None
 print("8. Самое большое число: ", repeating_numbers)
 
-min_number = min(numbers) if numbers else None
+min_number = min(num_list) if num_list else None
 print("9. Самое маленькое число: ", repeating_numbers)    
