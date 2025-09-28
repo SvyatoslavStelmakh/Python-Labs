34d74ce1ceaefe469e5f556ce7f87272ec24e59c
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-09-28 14:42:46 +0300

    Добавил обработку букв в верхнем регистре

--- Lab1/task2.py
+++ Lab1/task2.py
@@ -1,3 +1,3 @@
 str = input("Введите строку: ")
-result = str.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+result = str.replace("A", "").replace("E", "").replace("I", "").replace("O", "").replace("U", "").replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
 print(f"Строка без гласных(a, e, i, o, u): {result}")
