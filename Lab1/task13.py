c40b980d6217675d59c80c6058342736c1fe8ea4
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-10-07 14:03:31 +0300

    Перестал учитывать количество попыток в 13 задании

--- Lab1/task13.py
+++ Lab1/task13.py
@@ -1,21 +1,19 @@
 import random
 
 secret_number = random.randint(1, 100)
-attempts = 0
 
 print("Я загадал число от 1 до 100. Попробуй угадать!")
 
 while True:
     
     guess = int(input("Введите вашу догадку: "))
-    attempts += 1
      
     if guess < secret_number:
         print("Больше!")
     elif guess > secret_number:
         print("Меньше!")
     else:
-        print(f"Поздравляю! Вы угадали число {secret_number} за {attempts} попыток.")
+        print(f"Поздравляю! Вы угадали число {secret_number}.")
         break
    
         
