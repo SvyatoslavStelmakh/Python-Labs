7ac890e316bc260e59e889e6f73df8a0a469b64b
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-11-11 20:40:46 +0300

    дополнил задачу 1

--- Laba4/Part2/task1.py
+++ Laba4/Part2/task1.py
@@ -4,22 +4,22 @@ def expenses_comparison(expenses):
 
     expenses = np.array(expenses)
     
-    winter_months = {"december": 11, "january": 0, "febraury": 1}
-    summer_months = {"june": 5, "july": 6, "august": 7}
+    winter_months = [11, 0, 1]
+    summer_months = [5, 6, 7]
     
-    winter_expenses = np.sum(expenses[winter_months.values()])
-    summer_expenses = np.sum(expenses[summer_months.values()])
+    winter_expenses = np.sum(expenses[winter_months])
+    summer_expenses = np.sum(expenses[summer_months])
     
     print(f"Зимние месяцы:")
-    print(f"декабрь: {expenses[winter_months["december"]]}")
-    print(f"январь: {expenses[winter_months["january"]]}")
-    print(f"февраль: {expenses[winter_months["febraury"]]}")
+    print(f"декабрь: {expenses[11]:.2f}")
+    print(f"январь: {expenses[0]:.2f}")
+    print(f"февраль: {expenses[1]:.2f}")
     print(f"Общие расходы зимой: {winter_expenses:.2f}")
     
     print(f"Летние месяцы: ")
-    print(f"июнь: {expenses[summer_months["june"]]}")
-    print(f"июль: {expenses[summer_months["july"]]}")
-    print(f"август: {expenses[summer_months["august"]]}")
+    print(f"июнь: {expenses[5]:.2f}")
+    print(f"июль: {expenses[6]:.2f}")
+    print(f"август: {expenses[7]:.2f}")
     print(f"Общие расходы летом: {summer_expenses:.2f}")
     
     if winter_expenses > summer_expenses:
@@ -29,5 +29,9 @@ def expenses_comparison(expenses):
     else:
         print("Расходы равны в оба периода")
 
-random_expenses = np.random.normal(5000, 1000, 12)
+    print(f"Номер месяца с наибольшими тратами: {np.argmax(expenses) + 1}")
+
+random_expenses = np.random.normal(80, 50, 12)
 print(random_expenses)
+
+expenses_comparison(random_expenses)
