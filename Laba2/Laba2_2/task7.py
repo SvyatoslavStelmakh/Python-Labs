39a2eba410a62c8bb21cc68e929737e91f8f08e2
Mr. Svyatoslav <svyatolsavstelmakh@gmail.com>
2025-11-17 10:03:12 +0300

    добавил пользовательский ввод

--- Laba2/Laba2_2/task7.py
+++ Laba2/Laba2_2/task7.py
@@ -25,15 +25,16 @@ def merge_sorted_list(list1, list2):
     return result
 
 input_data = input("Введите первый список через пробел: ")
-list1 = sorted(input_data.split())
+list1 = map(int, input_data.split())
+list1 = sorted(list1)
 
 input_data = input("Введите второй список через пробел: ")
-list2 = sorted(input_data.split())
+list2 = map(int, input_data.split())
+list2 = sorted(list2)
 
 print("Примеры работы функции")
 merged = merge_sorted_list(list1, list2)
 print(f"list1: {list1}")
 print(f"list2: {list2}")
 print(f"merged: {merged}")
-
 
